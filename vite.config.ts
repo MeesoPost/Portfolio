d936f17fe6d386361f5c9503226f5eac09b90201
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2024-11-11 19:00:38 +0100

    feat: :sparkles: update to use swc

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,58 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   css: {
     modules: {
       localsConvention: 'camelCase',
     },
   },
+  esbuild: false,
+  optimizeDeps: {
+    exclude: ['@swc/core'],
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+        '.ts': 'tsx',
+      },
+    },
+  },
+  build: {
+    target: 'es2022',
+    cssCodeSplit: false,
+    reportCompressedSize: false,
+  },
+  plugins: [
+    react(),
+    {
+      name: 'swc',
+      async transform(code, id) {
+        if (!/\.(jsx?|tsx?)$/.test(id) || /node_modules/.test(id)) return;
+
+        const swc = await import('@swc/core');
+        const result = await swc.transform(code, {
+          filename: id,
+          sourceMaps: true,
+          jsc: {
+            parser: {
+              syntax: 'typescript',
+              tsx: true,
+            },
+            target: 'es2022',
+            minify: {
+              compress: false,
+              mangle: false,
+            },
+          },
+          module: {
+            type: 'es6',
+          },
+          isModule: true,
+          minify: false,
+        });
+
+        return result;
+      },
+      enforce: 'pre',
+    },
+  ],
 });
